6297b49f1b8ca981391bd4fbb23a5784df4b7acb
srithanandra <srithanandra@sfhs.com>
2024-01-20 17:45:14 -0800

    added shooter PIDs

--- src/main/cpp/Shooter.cpp
+++ src/main/cpp/Shooter.cpp
@@ -1,19 +1,30 @@
 #include "Shooter.h"
 
+
 void Shooter::initShooter() {
     bottomMotor->RestoreFactoryDefaults();
     topMotor->RestoreFactoryDefaults();
     bottomMotor->SetInverted(true); //check later
     topMotor->SetInverted(false);
+
+    angleControl.SetP(anglekP); //change these
+    angleControl.SetI(anglekI);
+    angleControl.SetD(anglekD);
+    shooterControl.SetP(shooterkP);
+    shooterControl.SetI(shooterkI);
+    shooterControl.SetD(shooterkD);
     shooterThread = std::thread(&run, this);
 
 }
 
-void Shooter::run() { 
+void Shooter::run() {
 
-    if (stopMotor==true) {
+    if (stopShooterMotor==true) {
         bottomMotor->StopMotor();
         topMotor->StopMotor();
+    }
+    else if (stopAngleMotor==true) {
+        angleMotor->StopMotor();
     }
     else {
         enableMotors();
@@ -24,9 +35,105 @@ void Shooter::run() {
 }
 
 void Shooter::stopMotors() {
-    stopMotor = true;
+    //stopMotor = true;
 }
 
 void Shooter::enableMotors() {
-    stopMotor = false;
+    //stopMotor = false;
 }
+
+void Shooter::setAngleSetpoint(float setpoint) {
+
+}
+
+void Shooter::setMotorVelocitySetpoint(float setpoint) {
+
+}
+
+bool Shooter::isFinished(float percentageBound){
+    double velocity = shooterEncoder.GetVelocity();
+    bool reachedVelocity = (velocity < (motorVelocitySetpoint * (1 + percentageBound))) && (velocity > (motorVelocitySetpoint * (1 - percentageBound)));
+
+    double angle = angleEncoder.GetPosition();
+    bool reachedAngle = (angle < (angleSetpoint * (1 + percentageBound))) && (angle > (angleSetpoint * (1 - percentageBound)));
+
+    return (reachedAngle && reachedVelocity);
+}
+
+double Shooter::getMotorVelocity() {
+    float velocity = shooterEncoder.GetVelocity();
+    return velocity;
+}
+
+double Shooter::getAnglePosition() {
+    double anglePosition = angleEncoder.GetPosition();
+    return anglePosition;
+}
+
+
+/*
+// Set the setpoint (target angle in encoder units)
+    double targetAngle = 90.0; // Replace with your desired target angle
+    armPIDController.SetReference(targetAngle, rev::ControlType::kPosition);
+
+    // Loop to periodically update the control
+    while (true) {
+        // You can add other logic or wait time here
+        // ...
+
+        // Read current encoder position
+        double currentAngle = armEncoder.GetPosition();
+
+        // Print current and target angles for debugging
+        std::cout << "Current Angle: " << currentAngle << ", Target Angle: " << targetAngle << std::endl;
+
+        // You may want to add a condition to break out of the loop when the arm reaches the target
+        // if (fabs(currentAngle - targetAngle) < tolerance) {
+        //     break;
+        // }
+    }
+
+    // Disable the PID controller
+    armPIDController.SetReference(0, rev::ControlType::kVoltage);
+
+    return 0;
+}*/
+
+
+
+
+
+/*
+float SwerveModule::getSteerAngleSetpoint()
+{
+    return steerAngleSetpoint;
+}
+
+/* Takes in input from 0 - 2pi
+ * 0 is the right, goes counterclockwise
+ * Not tested
+ */
+/*void SwerveModule::setSteerAngleSetpoint(float setpt)
+{
+    steerAngleSetpoint = setpt;
+}
+
+/**
+ * Untested, I've never used it
+ */
+/*void SwerveModule::setDrivePositionSetpoint(float setpt)
+{
+    drivePositionSetpoint = setpt;
+    driveMode = POSITION;
+}
+
+/**
+ * Set the drive motor velocity setpoint to input RPM
+ * Max RPM is 5700
+ */
+/*void SwerveModule::setDriveVelocitySetpoint(float setpt)
+{
+    driveVelocitySetpoint = setpt;
+    driveMode = VELOCITY;
+}
+*/
--- src/main/include/Shooter.h
+++ src/main/include/Shooter.h
@@ -2,21 +2,49 @@
 #include "geometry/Rotation2d.h"
 #include "geometry/Translation2d.h"
 #include <rev/CANSparkmax.h>
-#include <frc/shuffleboard/Shuffleboard.h>
 #include <thread>
+#include <rev/SparkPIDController.h>
+#include <rev/SparkRelativeEncoder.h>
+#include <frc/smartdashboard/SmartDashboard.h>
 
 #define bottomMotorID 0 //change later
 #define topMotorID 1
+#define angleMotorID 2
 
-class Shooter 
-{
+#define revkP 6e-5 //rev basic settings
+#define revkI 1e-6
+#define revkFF 0.000015
+#define revkMaxOutput 1.0
+#define revkMinOutput -1.0
+#define anglekP 0 //anglePID
+#define anglekI 0
+#define anglekD 0
+#define shooterkP 0 //shooterPID
+#define shooterkI 0
+#define shooterkD 0
+
+class Shooter {
     rev::CANSparkMax *bottomMotor = new rev::CANSparkMax(bottomMotorID, rev::CANSparkMax::MotorType::kBrushless);
     rev::CANSparkMax *topMotor = new rev::CANSparkMax(topMotorID, rev::CANSparkMax::MotorType::kBrushless);
+    rev::CANSparkMax *angleMotor = new rev::CANSparkMax(angleMotorID, rev::CANSparkMax::MotorType::kBrushless);
     std::thread shooterThread;
-    bool stopMotor = false;
+    rev::SparkPIDController angleControl;
+    rev::SparkPIDController shooterControl;
+    rev::SparkRelativeEncoder angleEncoder;
+    rev::SparkRelativeEncoder shooterEncoder;
+
+    bool stopAngleMotor = false;
+    bool stopShooterMotor = false;
+    float angleSetpoint;
+    float motorVelocitySetpoint;
+
     void initShooter();
     void run();
     void enableMotors();
     void stopMotors();
-
+    void setAngleSetpoint(float setpoint);
+    void setMotorVelocitySetpoint(float setpoint);
+    double getAnglePosition();
+    double getMotorVelocity();
+    bool isFinished(float percentageBound);
 };
