fa084679ff586b0906fdbab24cdc9803959440ea
sunnythecode <45048308+sunnythecode@users.noreply.github.com>
2024-01-31 19:47:53 -0800

    header file complete please fill in cpp file for intake

---
+++ src/main/cpp/Intake.cpp
@@ -0,0 +1,17 @@
+#include "Intake.h"
+
+void Intake::init()
+{
+    intakeMotor->RestoreFactoryDefaults();
+    intakeMotor->SetSmartCurrentLimit(intakeCurrentLimit);
+
+}
+
+void intake()
+{
+
+}
+
+void exhaust() {
+
+}
