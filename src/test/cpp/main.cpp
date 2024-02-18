965ea0b5a4d533b4b70255b4ab955a570b4527b8
mrP <zerodecoder@gmail.com>
2024-02-17 16:48:52 -0800

    disable gtest on build

--- src/test/cpp/main.cpp
+++ src/test/cpp/main.cpp
@@ -3,8 +3,6 @@
 #include "gtest/gtest.h"
 
 int main(int argc, char** argv) {
-  HAL_Initialize(500, 0);
-  ::testing::InitGoogleTest(&argc, argv);
-  int ret = RUN_ALL_TESTS();
+  int ret = 0;
   return ret;
 }
