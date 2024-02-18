4d86fd15243f5d031f637b9399aed10aef11b9f2
ab3131 <aravb3@gmail.com>
2024-02-17 16:12:55 -0800

    init & added velocity, enable and disable functions

---
+++ src/main/cpp/Index.cpp
@@ -0,0 +1,29 @@
+#include "Index.h"
+
+void Index::enableIndex(){
+    indexPID.SetD(indexD, indexMotorID);
+    indexPID.SetP(indexP, indexMotorID);
+    indexPID.SetI(indexI, indexMotorID);
+    setVelocity(maxRPM);
+    indexPID.SetReference(speed, rev::CANSparkBase::ControlType::kVelocity);
+    
+
+    indexMotor.Set(speed/maxRPM);
+    
+}
+
+void Index::disableIndex(){
+    setVelocity(0);
+    indexPID.SetReference(speed,rev::CANSparkBase::ControlType::kVelocity);
+
+}
+
+void Index::setVelocity(double vel){
+    speed = vel;
+}
+
+void Index::setDistance(){
+    
+}
+
+
---
+++ src/main/include/Index.h
@@ -0,0 +1,23 @@
+#include <rev/SparkPIDController.h>
+#include <rev/CANSparkMax.h>
+#include <rev/SparkRelativeEncoder.h>
+
+
+class Index{
+    double speed;
+    double indexP;
+    double indexI;
+    double indexD;
+    int indexMotorID;
+    int maxRPM = 5700;
+
+    rev::SparkPIDController indexPID;
+    rev::CANSparkMax indexMotor;
+    rev::SparkRelativeEncoder indexEncoder;
+
+    void enableIndex(){}
+    void disableIndex(){}
+    void setVelocity(double vel){}
+    void setDistance(){}
+};
+
